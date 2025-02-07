ec7307aeaebde18ca05a997b342a5884ce6c1155
Neetka <neetka@Neetkas-MacBook-Air.local>
2025-02-07 14:50:32 +0530

    intiated

---
+++ backend/types.js
@@ -0,0 +1,20 @@
+const zod = require("zod");
+/*
+{
+title: String
+description: string
+}
+{
+id: String.
+} */
+const createTodo = zod.object({
+    title: zod.string(),
+    description: zod.string()
+})
+const updateTodo = zod.object({
+    id: zod.string(),
+})
+module.exports = {
+    createTodo: createTodo,
+    updateTodo: updateTodo
+}
